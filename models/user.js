1928252d6f5c9b2e796d92737be29139325a8484
Desmond <desmond@bluewaterads.com>
2018-09-07 20:19:55 -0700

    updates

--- models/user.js
+++ models/user.js
@@ -2,31 +2,20 @@ var bcrypt = require('bcrypt-nodejs');
 
 module.exports = function(sequelize, DataTypes) {
     const User = sequelize.define('Users', {
-        annotation_id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        firstName: {
-            type: DataTypes.DATE,
-            field: 'first_name'
-        },
-        lastName: {
-            type: DataTypes.DATE,
-            field: 'last_name'
-        },
-        email: DataTypes.STRING,
+        username: DataTypes.STRING,
         password: DataTypes.STRING
     }, {
         freezeTableName: true,
-        instanceMethods: {
-            generateHash(password) {
-                return bcrypt.hash(password, bcrypt.genSaltSync(8));
-            },
-            validPassword(password) {
-                return bcrypt.compare(password, this.password);
-            }
-        }
+hooks: {
+	beforeCreate: user => {
+		const salt = bcrypt.genSaltSync();
+		user.password = bcrypt.hashSync(user.password, salt);
+	        },
+	beforeUpdate: user =>{ 
+		const salt = bcrypt.genSaltSync();
+		user.password = bcrypt.hashSync(user.password, salt);
+		}
+	}
     });
 
     return User;
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,23 +4,28 @@ require('../../config/passport')(passport);
 var express = require('express');
 var jwt = require('jsonwebtoken');
 var router = express.Router();
-var User = require("../../models/user");
+var db = require("../../models");
 
 router.post('/register', function(req, res) {
   if (!req.body.username || !req.body.password) {
     res.json({success: false, msg: 'Please pass username and password.'});
   } else {
-    var newUser = new User({
-      username: req.body.username,
-      password: req.body.password
-    });
-    // save the user
-    newUser.save(function(err) {
-      if (err) {
-        return res.json({success: false, msg: 'Username already exists.'});
-      }
-      res.json({success: true, msg: 'Successful created new user.'});
-    });
+    // var newUser = new User({
+    //   username: req.body.username,
+    //   password: req.body.password
+    // });
+    // // save the user
+    // newUser.save(function(err) {
+    //   if (err) {
+    //     return res.json({success: false, msg: 'Username already exists.'});
+    //   }
+    //   res.json({success: true, msg: 'Successful created new user.'});
+    // });
+   
+    db.Users.create(req.body)
+          .then(dbAuthor => res.status(201).send(dbAuthor))
+          .catch(error => res.status(400).send(error));
+
   }
 });
 
